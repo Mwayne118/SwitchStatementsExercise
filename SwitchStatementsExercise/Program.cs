059d010c3222f4a3bdabd4ea86f048f6e88ff013
Matthew Hendrix <hendrixm290@gmail.com>
2020-08-25 19:11:25 -0400

    Complete

---
+++ SwitchStatementsExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SwitchStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is awesome!!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science was cool but Chemistry sucked.");
+                    break;
+                case "English":
+                    Console.WriteLine("Nice, do you know Queen's English?");
+                    break;
+                case "Geography":
+                    Console.WriteLine("Now that was my favorite subject also!!");
+                    break;
+
+
+
+                default:
+                    Console.WriteLine("Ah, I have never taken that subject before.");
+                    break;
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
